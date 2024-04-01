bab135db3e648e8c580c154bba974ff547de31a6
Marcel Muslija <marcel.muslija@gmail.com>
2024-04-01 15:55:08 +0200

    Setup db connection

--- surl-server/internal/config/config.go
+++ surl-server/internal/config/config.go
@@ -7,12 +7,22 @@ import (
 )
 
 type Config struct {
-	Port string
+	Port   string
+	DbHost string
+	DbPort string
+	DbName string
+	DbUser string
+	DbPass string
 }
 
 func Get() *Config {
 	godotenv.Load()
 	return &Config{
-		Port: ":" + os.Getenv("SERVER_PORT"),
+		Port:   ":" + os.Getenv("SERVER_PORT"),
+		DbHost: os.Getenv("DB_HOST"),
+		DbPort: os.Getenv("DB_PORT"),
+		DbName: os.Getenv("DB_NAME"),
+		DbUser: os.Getenv("DB_USER"),
+		DbPass: os.Getenv("DB_PASS"),
 	}
 }
---
+++ surl-server/internal/db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"surl-server/internal/config"
+)
+
+func Connect(c *config.Config) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
+		c.DbHost,
+		c.DbPort,
+		c.DbUser,
+		c.DbPass,
+		c.DbName)
+
+	DB, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return DB, nil
+}
